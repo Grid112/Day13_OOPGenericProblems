5731a3b94deb46a6c7067cf4de253c2bfa46cfc2
Grid112 <sumitware1999@gmail.com>
2021-09-27 22:04:52 +0530

    Using Compare To Method TO Find Maximum Float

--- OOPGenericProblems.java
+++ OOPGenericProblems.java
@@ -9,11 +9,23 @@ public class OOPGenericProblems {
         if(c.compareTo(max)>0){
             max = c;
         }
-        System.out.print("max"+max);
+        System.out.print("Maximum Integer Is: "+max);
+    }
+    public static void floatMax(Float a, Float b, Float c){
+        Float max = a;
+        if(b.compareTo(max)>0){
+            max = b;
+        }
+        if(c.compareTo(max)>0){
+            max = c;
+        }
+        System.out.print("Maximum Float Is: "+max);
     }
 
     public static void main(String[] args) {
 	System.out.println("Welcome To OOP Generic Problems");
         intergerMax(8,9,6);
+        System.out.println();
+        floatMax(1.2F,4.5F,6.8F);
     }
 }
